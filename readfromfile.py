e69f57d3c81e230984af14c4c039d6f121586a20
Shuai Li <shuai_lee@outlook.com>
2019-01-17 21:09:48 -0700

    estimation of time to store the system events in plaintex

--- readfromfile.py
+++ readfromfile.py
@@ -1,3 +1,25 @@
+import time
+def readlines():
+    num_lines = sum(1 for line in open('receiving.txt'))
+    print(num_lines)
+# import hashlib
+# import os
+#
+# def hash(key, index):
+#     return hashlib.sha256(bytes(key)+bytes(index)).hexdigest()
+#
+#
+# print(len(hash(os.urandom(16),0)))
 
-num_lines = sum(1 for line in open('receiving.txt'))
-print(num_lines)
+
+def read():
+    S=[]
+    start_time = time.time()
+    with open("loggingevents_AES.txt", "rb") as ins:
+        for line in ins:
+            S.append(line)
+            #print(S[i])
+
+    print ("%s seconds to read " % (time.time() - start_time))
+
+read()
