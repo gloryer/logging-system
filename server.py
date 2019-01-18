f51b6aeb19891edbc8dcc2cb1872605cbbdd4114
Shuai Li <shuai_lee@outlook.com>
2019-01-17 21:08:49 -0700

    adding socket time out

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 
 import socket
+import time
+
 
 
 UDP_IP = "127.0.0.1"
@@ -10,9 +12,19 @@ serversock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)  # UDP
 
 serversock.bind((UDP_IP, UDP_PORT))
 
+
+timeout = time.time() + 3600
 while True:
 #for i in range(0.1):
-   data, addr = serversock.recvfrom(168820736)  # buffer size is 1024 bytes
-   with open('receiving.txt', 'a') as f:
-      f.write("%s \n"%data)
+     if time.time() > timeout:
+        break
+     data, addr = serversock.recvfrom(168820736)  # buffer size is 1024 bytes
+     with open('receiving.txt', 'a') as f:
+         f.write("%s \n"%data)
+
+
+
+serversock.close()
+
+
 
